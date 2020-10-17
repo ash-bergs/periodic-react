69eb69c9a8fcbd0aa93b262834ce0eeea8dadef3
Ash <ashley.bergsma42@gmail.com>
2020-10-16 21:47:44 -0700

    framer-motion added to the project. It added some nice springy hover animations for the elements!

---
+++ periodic-table/src/data/colorMap.js
@@ -0,0 +1,12 @@
+const colorMap = {
+    "noble gas": "#FFBC42", 
+    "alkaline earth metal": "#EC674E", 
+    "diatomic nonmetal": "#D81159", 
+    "alkali metal": "#8F2D56", 
+    "transition metal": "#58586B", 
+    "post-transition metal": "#218380", 
+    lanthanide: "#4AABAF", 
+    metalloid: "#73D2DE"
+}; 
+
+export default colorMap;
